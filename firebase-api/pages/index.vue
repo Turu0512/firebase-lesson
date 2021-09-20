9b98bc54bb813a691fd78d2310893d678b790fb0
Turu0512 <hinataptyan@gmail.com>
2021-09-21 06:39:44 +0900

    初回コミット

---
+++ firebase-api/pages/index.vue
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+    res.send("Hello);
+});
+
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
